5dc7d57b52d566e9792662fff639a17f0393e65d
mengjin sun <105015271+mengjin193@users.noreply.github.com>
2025-11-30 17:40:15 -0500

    Add files via upload

---
+++ Small Pages/JSTutorial.js
@@ -0,0 +1,17 @@
+function getDate()	{
+	document.getElementById('currentDate').innerHTML = Date();
+}
+
+// document.getElementById('').src='' [show picture]
+// document.getElementById('').style.fontSize = "" [change CSS]
+// document.getElementById('').style.dispay = "none" [hide HTML elements];
+// document.getElementById('').style.dispay = "block" [show HTML elements];
+
+var x = 3;
+var y = 3;
+document.write(x+y); //should only for testing
+window.alert("hello");
+console.log("for debugging");
+
+var meal = ["breakfast", "lunch", "dinner"]; //arrary
+var person = {firstname:"Mengjin", lastname:"Sun", age:22}; //object
